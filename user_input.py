773f81c3396d24ad1808f45c895cca80844120b8
James Alex Akande <jamesakande007@gmail.com>
2024-03-05 15:11:34 +0100

    This is Challenge One

---
+++ user_input.py
@@ -0,0 +1,19 @@
+# user_input.py
+#using the input function
+# Ask for the user's name
+name = input("Can you tell me your name, kiddo? ")
+
+# Ask for the user's age
+age = input("How old are you, chap? ")
+
+# Ask for the user's location
+location = input("Where do you live, mate? ")
+
+# Personalized message
+print("Hello {}, you are {} years old and live in {}.".format(name, age, location))
+
+a = (1, 2, 3)
+
+b = list(a)
+
+print(b)
